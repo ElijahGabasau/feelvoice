f7f433087f39abc93a384985cc9545619a5f03c9
Elijah <elijah.gabasau@gmail.com>
2021-03-01 18:11:17 +0300

    customers wishes implied

--- client/src/components/MenuFigure.js
+++ client/src/components/MenuFigure.js
@@ -1,11 +1,7 @@
-import icons from '../img/icons.svg';
-
 function MenuFigure() {
   return (
     <div className="navigation__figure navigation__figure--box">
-      <svg className="navigation__figure navigation__figure--cross">
-        <use href={icons + "#cross"} />
-      </svg>
+        <img className="navigation__logo navigation__figure" src={require('../img/logo-solid.png').default} />
     </div>
   )
 }
